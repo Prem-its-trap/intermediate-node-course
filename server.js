191f345909131f8ecd7e018f1f8ad01b88b7817a
Prem-its-trap <58662853+Prem-its-trap@users.noreply.github.com>
2021-04-21 02:50:25 +0530

    get route added

--- server.js
+++ server.js
@@ -15,14 +15,41 @@ app.listen(port, () => {
 
 // CREATE
 app.post("/users", (req, res) => {
-  // User.create()
+  User.create(
+    {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    },
+    (err, data) => {
+      if (err) {
+        res.json({ success: false, message: err });
+      } else if (!data) {
+        res.json({ success: false, message: "Not found" });
+      } else {
+        res.json({ success: true, data: data });
+      }
+    }
+  );
 });
 
+// route chaining
 app
   .route("/users/:id")
   // READ
   .get((req, res) => {
-    // User.findById()
+    const id = "607f3e595d6de62d2849b0d9";
+    // const id = req.params._id;
+    User.findById(id, (err, data) => {
+      if (err) {
+        res.json({ success: false, message: err });
+      } else {
+        res.json({
+          success: true,
+          data: data,
+        });
+      }
+    });
   })
   // UPDATE
   .put((req, res) => {
